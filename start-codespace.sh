bbeab4b52796a397a40ca6bc69de85dbff48ee60
mrbateau <153347761+mrbateau@users.noreply.github.com>
2024-06-27 19:08:11 +0700

    Create start-codespace

---
+++ start-codespace.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Start your Codespace URL
+echo "Starting Codespace..."
+# Replace this with the actual command or script to start your Codespace
+curl -X POST https://codespaces-api-url/start
